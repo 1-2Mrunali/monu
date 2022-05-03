1d1cebc39d6496bfe7785ea68a8a65ceb90a9bfe
1-2Mrunali <mrunali.gurpude.ee@ghrce.raisoni.net>
2022-05-03 14:01:09 +0530

    initial.commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let ctx = c.getContext("2d");
 let loadImage = (src, callback) => {
     let img = document.creatElement("img");
     img.onload = () => callback(img);
-    img.src = /images/idle.png;
+    img.src = src;
 };
 
 
@@ -16,7 +16,7 @@ let frames ={
     punch: [1, 2, 3, 4, 5, 6,7],
 };
 
-let loadImages = (callback) => {
+let loadImages =( (callback) => {
     let images = {idle:[], kick:[], punch:[]};
     let imagesToLoad = 0;
 
@@ -37,7 +37,7 @@ let loadImages = (callback) => {
         
         
     });
-};
+});
 
 let animate=(ctx, images, animation, callback)=>{
     images[animation].forEach((image, index)=>{
@@ -50,8 +50,50 @@ let animate=(ctx, images, animation, callback)=>{
     setTimeout(callback, images[animation].length*100);
 };
 
-loadImage((images)=>{
-    animate(ctx, images, "idle", ()=>{
-        console.log("done!");
+loadImage = (images)=>{
+   let selectedAnimation;
+    
+    if(queuedAnimations.length === 0){
+        selectedAnimation = "idle";
+    }else{
+        selectedAnimation = queuedAnimations.shift();
+    }
+   
+   
+        animate(ctx, images, selectedanimation, aux);
+    };
+    
+    aux();
+    
+    document.getElementById("kick").onclick=()=>{
+        queuedAnimations.push("kick");
+    };
+    
+    document.getElementById("punh").onclick=()=>{
+        queuedAnimation.push("punh");
+    };
+    
+    document.getElementById("idle").onclick=()=>{
+       queuedAnimation.push("idle");
+    };
+    
+    document.getElementById("backward").onclick=()=>{
+       queuedAnimation.push("backward");
+    };
+    
+    document.getElementById("block").onclick=()=>{
+        queuedAnimation.push("block");
+    };
+    
+    document.getElementById("forward").onclick=()=>{
+        queuedAnimation.push("forward");
+    };
+document.addElementListner("keyup", (event) => {
+    const key = event.key;
+    
+    if(key==="ArrowLeft"){
+        queuedAnimation.push("kick");
+    }else if(key === "ArrowRight"){
+        queuedAnimation.push("punch");
+    }
     });
-});
